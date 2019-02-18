54989999eb129bffb7d63816f38814b9c58b9f8d
Dylan Frankland <dylan@frankland.io>
2019-02-17 22:24:43 -0800

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![no_std]
 #![no_main]
 
-use panic_halt;
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use panic_halt;
 use stm32f3::stm32f303;
 
 #[entry]
@@ -18,7 +18,7 @@ fn main() -> ! {
     gpioc.moder.write(|w| w.moder13().output());
     gpioc.ospeedr.write(|w| w.ospeedr13().very_high_speed());
 
-    loop{
+    loop {
         gpioc.bsrr.write(|w| w.bs13().set_bit());
         asm::delay(2_000_000);
         gpioc.brr.write(|w| w.br13().set_bit());

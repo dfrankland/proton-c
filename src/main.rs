ba9edfa23372bb27b3cee71dd7d288bdf5c3f80d
Dylan Frankland <dylan@frankland.io>
2019-04-07 19:14:58 -0700

    Switch to using stm32f3xx-hal and cortex-m-rtfm

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,57 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
-use panic_halt;
-use stm32f3::stm32f303;
+extern crate panic_semihosting;
 
-#[entry]
-fn main() -> ! {
-    // get handles to the hardware
-    let peripherals = stm32f303::Peripherals::take().unwrap();
-    let gpioc = &peripherals.GPIOC;
-    let rcc = &peripherals.RCC;
+use embedded_hal::digital::OutputPin;
+use rtfm::{app, Instant};
+use stm32f3xx_hal::{
+    flash::FlashExt,
+    gpio::{gpioc, GpioExt, Output, PushPull},
+    rcc::RccExt,
+    time::U32Ext,
+};
 
-    // enable the GPIO clock for IO port C
-    rcc.ahbenr.write(|w| w.iopcen().set_bit());
-    gpioc.moder.write(|w| w.moder13().output());
-    gpioc.ospeedr.write(|w| w.ospeedr13().very_high_speed());
+const PERIOD: u32 = 2_000_000;
 
-    loop {
-        gpioc.bsrr.write(|w| w.bs13().set_bit());
-        asm::delay(2_000_000);
-        gpioc.brr.write(|w| w.br13().set_bit());
-        asm::delay(2_000_000);
+#[app(device = stm32f3xx_hal::stm32)]
+const APP: () = {
+    static mut LED: gpioc::PC13<Output<PushPull>> = ();
+
+    #[init(schedule = [led_on])]
+    fn init() -> init::LateResources {
+        let mut flash = device.FLASH.constrain();
+        let mut rcc = device.RCC.constrain();
+
+        rcc.cfgr
+            .hclk(8_u32.mhz())
+            .sysclk(48_u32.mhz())
+            .pclk1(24_u32.mhz())
+            .freeze(&mut flash.acr);
+
+        let mut gpioc = device.GPIOC.split(&mut rcc.ahb);
+        let led = gpioc
+            .pc13
+            .into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
+
+        schedule.led_on(Instant::now()).unwrap();
+
+        init::LateResources { LED: led }
     }
-}
+
+    #[task(schedule = [led_off], resources = [LED])]
+    fn led_on() {
+        resources.LED.set_high();
+        schedule.led_off(scheduled + PERIOD.cycles()).unwrap();
+    }
+
+    #[task(schedule = [led_on], resources = [LED])]
+    fn led_off() {
+        resources.LED.set_low();
+        schedule.led_on(scheduled + PERIOD.cycles()).unwrap();
+    }
+
+    extern "C" {
+        fn USART1_EXTI25();
+    }
+};
